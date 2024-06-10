14037e835898db9515eb4d8d8b4ad5894103b818
Jai Phookan <jaiphookan20@gmail.com>
2024-06-10 17:31:10 +1000

    Job Section fixes. Removed prior code.

--- job-listings/src/components/JobSection.js
+++ job-listings/src/components/JobSection.js
@@ -26,34 +26,3 @@ const JobSection = ({ title, jobs, onSave, onApply, onView }) => {
 };
 
 export default JobSection;
-
-// import JobCard from "./JobCard";
-
-// const JobSection = ({ title, jobs, onSave, onApply, onView }) => {
-//   return (
-//     // <div className="col-span-2 flex">
-//     <div className="col-span-2">
-//       <div>
-//         <div className="flex justify-between items-center mb-4 hover:cursor-pointer">
-//           <h2 className="text-3xl font-bold">{title}</h2>
-//           <a href="" className="text-indigo-600 text-md hover:underline">
-//             View all {title}
-//           </a>
-//         </div>
-//         <div className="bg-white rounded-lg shadow-md cursor-pointer">
-//           {jobs.map((job, index) => (
-//             <JobCard
-//               key={index}
-//               job={job}
-//               onSave={onSave}
-//               onApply={onApply}
-//               onView={onView}
-//             />
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default JobSection;
