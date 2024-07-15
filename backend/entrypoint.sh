bb9823d7c9fd86f70e95139989663ee20fa0bb70
Jai Phookan <jaiphookan20@gmail.com>
2024-07-15 22:46:11 +1000

    separated logic for 'development' and 'production' for hot-reload of both FE & BE

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,14 +1,32 @@
 #!/bin/sh
 
-# Wait for the database to be ready
 echo "Waiting for PostgreSQL..."
 while ! nc -z $DB_HOST 5432; do
   sleep 1
 done
 echo "PostgreSQL started"
 
-# Run migrations
 flask db upgrade
 
-# Start the application
-gunicorn --bind 0.0.0.0:4040 "app:create_app()"
+if [ "$FLASK_ENV" = "development" ]; then
+    echo "Starting Flask development server with hot reloading..."
+    python -m flask run --host=0.0.0.0 --port=4040 --reload
+else
+    echo "Starting Gunicorn production server..."
+    gunicorn --config gunicorn-config.py "app:create_app()"
+fi
+
+# #!/bin/sh
+
+# # Wait for the database to be ready
+# echo "Waiting for PostgreSQL..."
+# while ! nc -z $DB_HOST 5432; do
+#   sleep 1
+# done
+# echo "PostgreSQL started"
+
+# # Run migrations
+# flask db upgrade
+
+# # Start the application
+# gunicorn --bind 0.0.0.0:4040 "app:create_app()"
