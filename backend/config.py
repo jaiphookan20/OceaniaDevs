5b0b803cc56826d2d258ad7d3a632092f9abc112
Jai Phookan <jaiphookan20@gmail.com>
2024-07-16 00:23:02 +1000

    minor change

--- backend/config.py
+++ backend/config.py
@@ -22,5 +22,6 @@ TEST_DB_USER = os.getenv('TEST_DB_USER', 'jai')
 TEST_DB_PASSWORD = os.getenv('TEST_DB_PASSWORD', 'techboard')
 
 BASE_URL = os.getenv('BASE_URL', 'http://54.79.190.69')
+
 # Uncomment and use http://localhost during local docker setup ie not in AWS Lightsail
 # BASE_URL = os.getenv('BASE_URL', 'http://localhost') 
