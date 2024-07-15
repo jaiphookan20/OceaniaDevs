e73f3762b3b434becb2bd9d962903ff5a8a35b55
Jai Phookan <jaiphookan20@gmail.com>
2024-07-15 19:32:00 +1000

    changed bg color to fuchsia

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -35,7 +35,7 @@ const Header = () => {
   return (
     <header className="text-center py-12 mb-8 rounded-lg max-w-8xl mx-auto">
       {/* bg-[#c3f53c] */}
-      <div className="flex justify-between items-center bg-[#c3f53c]">
+      <div className="flex justify-between items-center bg-fuchsia-500">
         {/* <div className="flex justify-center items-center space-x-4"> */}
         <div className="grid grid-cols-3 grid-rows-2 gap-4">
           <img src={hero7} className="w-32 h-32" />
@@ -47,10 +47,12 @@ const Header = () => {
         </div>
         <div className="text-center rounded-md">
           <div className="flex">
-            <h1 className="text-6xl font-bold text-black">Find what's next</h1>
+            <h1 className="text-6xl font-bold text-lime-100 ">
+              Find what's next
+            </h1>
             <h1 className="text-6xl font-bold text-lime-500">:</h1>
           </div>
-          <p className="text-green-800 mt-5 text-1xl">
+          <p className="text-green-800 mt-5 text-1xl text-lime-200 font-semibold">
             THE BEST TECHNOLOGY JOBS, DOWN UNDER
           </p>
         </div>
