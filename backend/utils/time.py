f3a2d8c40a97804db9cde50119eb93a1d7f6af52
Jai Phookan <jaiphookan20@gmail.com>
2024-09-14 17:28:57 +1000

    time fix

--- backend/utils/time.py
+++ backend/utils/time.py
@@ -11,19 +11,19 @@ def get_relative_time(date_str):
     if delta.days <= 0:
         return 'Today'
     elif delta.days == 1:
-        return '1 day ago'
+        return '1d ago'
     elif delta.days < 7:
-        return f'{delta.days} days ago'
+        return f'{delta.days}d ago'
     elif delta.days < 14:
-        return '1 week ago'
+        return '1w ago'
     elif delta.days < 21:
-        return '2 weeks ago'
+        return '2w ago'
     elif delta.days < 28:
-        return '3 weeks ago'
+        return '3w ago'
     elif delta.days < 31:
-        return '4 weeks ago'
+        return '4w ago'
     elif delta.days < 62:
-        return '1 month ago'
+        return '1m ago'
     else:
         return f'{delta.days // 30} months ago'
     
