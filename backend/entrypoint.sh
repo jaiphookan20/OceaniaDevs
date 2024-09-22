b1beb624d79809d3e9f9605e579e47762aa55bc2
Jai Phookan <jaiphookan20@gmail.com>
2024-09-22 14:17:46 +1000

    trying to fix entrypoint.sh permission issue

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-# echo "Printing all environment variables:"
-# env
-
-# echo "Specific checks for Auth0 variables:"
-# echo "AUTH0_DOMAIN: $AUTH0_DOMAIN"
-# echo "AUTH0_CLIENT_ID: $AUTH0_CLIENT_ID"
-# echo "AUTH0_CLIENT_SECRET is set: $(if [ -n "$AUTH0_CLIENT_SECRET" ]; then echo "Yes"; else echo "No"; fi)"
-# echo "APP_SECRET_KEY is set: $(if [ -n "$APP_SECRET_KEY" ]; then echo "Yes"; else echo "No"; fi)"
+set -e
 
 # Wait for the database to be ready
 echo "Waiting for PostgreSQL..."
@@ -15,9 +8,6 @@ while ! nc -z $DB_HOST 5432; do
   sleep 1
 done
 echo "PostgreSQL started"
-
-# Wait an additional 10 seconds for the init script to complete
-sleep 10
 
 # Run database migrations
 echo "Running database migrations..."
@@ -31,4 +21,4 @@ sleep 5
 
 # Start the application
 echo "Starting the application..."
-gunicorn --config gunicorn_config.py "app:create_app()"
+exec gunicorn --config gunicorn_config.py "app:create_app()"
