31cb7107b27a2f81b68245f24015fb07d09d2b7b
Jai Phookan <jaiphookan20@gmail.com>
2024-09-22 16:40:28 +1000

    trying same without psql

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -19,8 +19,8 @@ fi
 # Use PGPASSWORD environment variable for psql
 export PGPASSWORD=$DB_PASSWORD
 
-# Use psql command directly
-psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c 'DROP TABLE IF EXISTS alembic_version;'
+# Use SQLAlchemy to drop the alembic_version table
+python -c "from sqlalchemy import create_engine; from sqlalchemy.sql import text; engine = create_engine('postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST/$DB_NAME'); with engine.connect() as conn: conn.execute(text('DROP TABLE IF EXISTS alembic_version'))";
 
 flask db current || {
     echo "No current revision. Initializing database..."
