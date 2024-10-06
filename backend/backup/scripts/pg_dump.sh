5962ac4cc67a1149a22198ed32a96554e8f9eea0
Jai Phookan <52240311+jaiphookan20@users.noreply.github.com>
2024-10-07 10:57:53 +1100

    Changed pg_dump.sh to use localhost instead of postgres (#25)
    * added db backup scripts
    
    * updated pg_dump.sh to use localhost instead of 'postgres'

--- backend/backup/scripts/pg_dump.sh
+++ backend/backup/scripts/pg_dump.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+set -x  # Enable debugging
+
 source /home/ubuntu/OceaniaDevs/backend/backup/config.env
+
+echo "BACKUP_DIR: $BACKUP_DIR"
+echo "DB_HOST: $DB_HOST"
+echo "POSTGRES_USER: $POSTGRES_USER"
+echo "POSTGRES_DB: $POSTGRES_DB"
 
 if [ "$FLASK_ENV" = "staging" ]; then
     POSTGRES_DB="${POSTGRES_DB}_staging"
 fi
 
-mkdir -p $BACKUP_DIR
+mkdir -p "$BACKUP_DIR"
 
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 
-PGPASSWORD=$POSTGRES_PASSWORD pg_dump -h $DB_HOST -U $POSTGRES_USER -d $POSTGRES_DB | gzip > $BACKUP_DIR/pg_dump_$TIMESTAMP.sql.gz
+PGPASSWORD=$DB_PASSWORD pg_dump -h localhost -U $DB_USER -d $DB_NAME | gzip > "$BACKUP_DIR/pg_dump_$TIMESTAMP.sql.gz"
 
-find $BACKUP_DIR -name "pg_dump_*.sql.gz" -type f -mtime +7 -delete
+find "$BACKUP_DIR" -name "pg_dump_*.sql.gz" -type f -mtime +7 -delete
