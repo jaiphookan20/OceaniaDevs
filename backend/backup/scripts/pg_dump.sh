c54a91b90eebb1e64cc4c6ade90b804365888ed2
Jai Phookan <jaiphookan20@gmail.com>
2024-10-07 11:35:14 +1100

    updated pg_dump file path

--- backend/backup/scripts/pg_dump.sh
+++ backend/backup/scripts/pg_dump.sh
@@ -17,6 +17,7 @@ mkdir -p "$BACKUP_DIR"
 
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 
-PGPASSWORD=$DB_PASSWORD pg_dump -h localhost -U $DB_USER -d $DB_NAME | gzip > "$BACKUP_DIR/pg_dump_$TIMESTAMP.sql.gz"
+# Use the full path to pg_dump
+PGPASSWORD=$DB_PASSWORD /usr/lib/postgresql/14/bin/pg_dump -h localhost -U $DB_USER -d $DB_NAME | gzip > "$BACKUP_DIR/pg_dump_$TIMESTAMP.sql.gz"
 
 find "$BACKUP_DIR" -name "pg_dump_*.sql.gz" -type f -mtime +7 -delete
