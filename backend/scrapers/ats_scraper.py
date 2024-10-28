d4984437896601feac54fdbae00ba14038b657e9
Jai Phookan <jaiphookan20@gmail.com>
2024-10-28 15:06:59 +1100

    fixing path issue

--- backend/scrapers/ats_scraper.py
+++ backend/scrapers/ats_scraper.py
@@ -1,6 +1,13 @@
 import os
+import sys
 from apify_client import ApifyClient
 from typing import List, Dict, Any, Tuple
+
+# Add the parent directory (backend/) to Python's path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
 from service.recruiter_service import RecruiterService
 from models import Company, db, Job
 import logging
